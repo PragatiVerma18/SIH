3b85fd799cf94e561aea66bb7719da17282c7590
PragatiVerma18 <itispragativerma@gmail.com>
2020-07-23 22:57:23 +0530

    added status

--- jobs/models.py
+++ jobs/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
 from accounts.models import User
-
-JOB_TYPE = (
-    ('1', "Full time"),
-    ('2', "Part time"),
-    ('3', "Internship"),
-)
 
 
 class Job(models.Model):
@@ -16,7 +9,7 @@ class Job(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField(blank=True)
     location = models.CharField(max_length=150)
-    type = models.CharField(choices=JOB_TYPE, max_length=10)
+    type = models.CharField(max_length=10)
     category = models.CharField(max_length=100)
     last_date = models.DateTimeField()
     company_name = models.CharField(max_length=100)
@@ -42,6 +35,7 @@ class Applicant(models.Model):
     job = models.ForeignKey(Job, on_delete=models.CASCADE,
                             related_name='applicants')
     applied_at = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=15)
 
     def __str__(self):
         return self.user.get_full_name()
